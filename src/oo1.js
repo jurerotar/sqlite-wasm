834bbc29147ddffb54c44010efd8c7fb11d52d5a
Jure Rotar <jrotar0@gmail.com>
2026-01-14 19:29:43 +0100

    test: test

---
+++ src/oo1.js
@@ -0,0 +1,6 @@
+// This file is constructed from partials from the SQLite repository.
+import './bin/partials/sqlite3-api-prologue.js';
+import './bin/partials/common/whwasmutil.js';
+import './bin/partials/jaccwabyt/jaccwabyt.js';
+import './bin/partials/sqlite3-api-glue.js';
+import './bin/partials/sqlite3-api-oo1.js';
